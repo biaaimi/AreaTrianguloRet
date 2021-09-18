dcc32e9041cd629d0bd45792aad44b6b0b0651b1
biaaimi <biagioni.aimi@gmail.com>
2021-09-18 20:40:30 -0300

    AreaTrianguloRet

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercicios_etec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Digite o valor da base triângulo ");
+            string Base = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nAgora, digite o valor da altura:");
+            string Altura = Console.ReadLine();
+
+            double areaTriangulo = ( Convert.ToDouble(Base)* Convert.ToDouble(Altura))/ 2;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"\nTotal área = {areaTriangulo}");
+
+
+            Console.ReadKey();
+            Console.ResetColor();
+            
+        }
+    }
+}
